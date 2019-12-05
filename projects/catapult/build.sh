f9232b245424caad64d9a953dcadb49ed9cb159c
WayonB <wayonb@gmail.com>
2019-12-05 00:29:01 -0500

    add config files to test

--- projects/catapult/build.sh
+++ projects/catapult/build.sh
@@ -6,17 +6,22 @@ cd _build
 cmake_args=()
 cmake_args+=("-DCMAKE_INSTALL_PREFIX=${OUT}")
 cmake_args+=("-DCMAKE_BUILD_TYPE=RelWithDebInfo")
-cmake_args+=("-DUSE_KECCAK=TRUE")
+#cmake_args+=("-DUSE_KECCAK=TRUE")
 
 #
 #cmake_args+=("-DUSE_SANITIZER= sanitizer name ")
 #
 
 cmake_args+=("-DENABLE_TESTS=OFF")
+cmake_args+=("-DENABLE_FUZZING=ON")
 
 cmake "${cmake_args[@]}" ..
 make publish
 make -j8 server plugins extensions 
 make install
-mkdir $OUT/resources
-cp $SRC/catapult-server/resources/* $OUT/resources 
+
+# copy the fuzz target
+cp $OUT/bin/catapult.tools.pipeline $OUT
+
+# copy the rocksdb lib
+cp /usr/local/lib/librocksdb.so* $OUT
